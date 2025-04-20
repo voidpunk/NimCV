32cb80480ececa3fd5223513651a2f0c27e4b074
Nil <voidpunk.glitch@gmail.com>
2025-04-21 00:54:30 +0200

    Refactored utils

--- generator/parsers/utils.nim
+++ generator/parsers/utils.nim
@@ -1,15 +1,15 @@
 import regex
 
-proc stripComments*(content: var string) =
+proc stripComments*(content: string): string =
   ## Removes C/C++ comments using precompiled regex
   const
     blockComment = re2"""/\*[^*]*\*+(?:[^/*][^*]*\*+)*/"""  # Handles /* ... */ (even multiline)
     lineComment  = re2"""//[^\n]*"""                        # Handles // until newline
     whitespaces  = re2"""\s{1,}"""                          # Handles all whitespaces (' ', '\t', '\n')
     ifndefBlock  = re2"""#ifndef.*?#endif.*?"""             # Handles #ifndef ... #endif blocks inline
-  # Replace in a single pass (faster than two separate replaces)
-  content = content.replace(blockComment, "" )  
-  content = content.replace(lineComment,  "" )
-  content = content.replace(whitespaces,  " ")
-  content = content.replace(ifndefBlock,  "" )
+  result = content
+    .replace(blockComment, "" )  
+    .replace(lineComment,  "" )
+    .replace(whitespaces,  " ")
+    .replace(ifndefBlock,  "" )
   # This last must be called after stripping whitespaces, since it only works inline
